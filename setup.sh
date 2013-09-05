e7f6acf2a3a7ef3f18dda8144012f46d750b5e8f
PanMonkey <lunkhead10@gmail.com>
2013-09-06 00:44:56 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-#DOT_FILES=( .zshrc .emacs.d )
-
-#for file in ${DOT_FILES[@]}
-#do
-#    if [ -a $HOME/$file ]; then
-#        if [ -L $HOME/$file ]; then
-#            echo "既にシンボリックリンクが存在します: $file"
-#        elif [ -d $HOME/$file ]; then
-#            echo "既にディレクトリが存在します: $file"
-#        else
-#            echo "既にファイルが存在します: $file"
-#        fi
-#    else
-#        ln -s $HOME/dotfiles/$file $HOME/$file
-#        echo "シンボリックリンクを貼りました: $file"
-#    fi
-#done
-
 cd `dirname $0`
 CURRENT_DIR=$PWD
 WORKING_DIR=$CURRENT_DIR/tmp
