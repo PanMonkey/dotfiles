1fff4931fb60656b8c287023c08ba88067df8a86
lunkhead10 <lunkhead10@gmail.com>
2013-09-17 22:33:50 +0900

    -

--- .emacs.d/elpa/ruby-block-20111101.1152/ruby-block-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; ruby-block-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/ruby-block-20111101.1152/ruby-block-pkg.el"
-;;;;;;  "../../../../.emacs.d/elpa/ruby-block-20111101.1152/ruby-block.el")
-;;;;;;  (21048 21550 506998))
-
-;;;***
-
-(provide 'ruby-block-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ruby-block-autoloads.el ends here
